7a47a45dc18b3d723f14072255a5a90bc937fa82
sergiokno <yonyarleyorejuela@gmail.com>
2025-11-26 20:29:59 -0500

    Initial: proyecto corregido listo para Render

--- apprender/apps.py
+++ apprender/apps.py
@@ -1,6 +1,5 @@
 from django.apps import AppConfig
 
-
 class ApprenderConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apprender'
+    default_auto_field='django.db.models.BigAutoField'
+    name='apprender'
--- apprender/views.py
+++ apprender/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
-def index(request):
-    return render(request, 'apprender/index.html')
-
+def home(request):
+    return HttpResponse('Proyecto desplegado correctamente en Render 🚀')
+    
